8bbfc92a4958dd1e266aa6f785f6b037a84df108
Hengjian Jia <henryjia18@yahoo.com>
2015-05-28 22:35:37 +0100

    Transforms the repository into a machine learning library built with OpenCV.

--- main.cpp
+++ main.cpp
@@ -1,76 +1,9 @@
 #include <iostream>
-#include <Eigen/Dense>
-#include <chrono>
-#include <lapackpp.h>
-#include <armadillo>
-#include <fstream>
-#include <iomanip>
 
 #include "cvnn.h"
 
-using namespace Eigen;
-using namespace arma;
-
-double testEigen()
-{
-	auto start = chrono::steady_clock::now();
-	MatrixXd a = MatrixXd::Random(300000, 80);
-	MatrixXd b = MatrixXd::Random(80, 30);
-	MatrixXd result(300000, 30);
-	result = a * b;
-	auto end = chrono::steady_clock::now();
-	auto elapsed = end - start;
-	return chrono::duration <double> (elapsed).count();
-}
-
-double testLapack()
-{
-	auto start = chrono::steady_clock::now();
-	LaGenMatDouble a = LaGenMatDouble::rand(300000, 80);
-	LaGenMatDouble b = LaGenMatDouble::rand(80, 30);
-	LaGenMatDouble result(300000, 30);
-	Blas_Mat_Mat_Mult(a, b, result, 1.0, 0.0);
-	auto end = chrono::steady_clock::now();
-	auto elapsed = end - start;
-	return chrono::duration <double> (elapsed).count();
-}
-
-double testArmadillo()
-{
-	auto start = chrono::steady_clock::now();
-	mat a = randu<mat>(300000, 80);
-	mat b = randu<mat>(80, 30);
-	mat c = a * b;
-	auto end = chrono::steady_clock::now();
-	auto elapsed = end - start;
-	return chrono::duration <double> (elapsed).count();
-}
-
-double testOpenCV()
-{
-	auto start = chrono::steady_clock::now();
-	cv::Mat a(300000, 80, CV_64F);
-	cv::Mat b(80, 30, CV_64F);
-	cv::randu(a, 0.0, 1.0);
-	cv::randu(b, 0.0, 1.0);
-	cv::Mat c = a * b;
-	auto end = chrono::steady_clock::now();
-	auto elapsed = end - start;
-	return chrono::duration <double> (elapsed).count();
-}
-
 int main()
 {
-	cout << "Matrix Multiplication Tests" << endl;
-	cout << "Eigen Start" << endl;
-	cout << testEigen() << " s" << endl;
-	cout << "Lapack Start" << endl;
-	cout << testLapack() << " s" << endl;
-	cout << "Armadillo Start" << endl;
-	cout << testArmadillo() << " s" << endl;
-	cout << "OpenCV Start" << endl;
-	cout << testOpenCV() << " s" << endl;
-
 	cout << "Neural Network Gradient Descent Tests" << endl;
 	cvnn nn;
 	cout << "Read CSV" << endl;
